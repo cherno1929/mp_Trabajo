41e74b47529f5759bcf50d81313368b015ccf6d3
Cherno Valdor <y.labchiri.2021@alumnos.urjc.es>
2023-04-15 18:51:22 +0200

    Actualizacion FileController

--- src/FileController.java
+++ src/FileController.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,17 +9,18 @@ public class FileController {
     //Atributes
     private String locationUsuario = "Ficheros_app/Usuarios";
     private String locationDesafios = "Ficheros_app/Desafios";
-    private String localPersonajes = "Ficheros_app/Personaje_Usuario";
+    private String localPersoanjes = "Ficheros_app/Personaje_Usuario";
     private String LocalAtributos = "Ficheros_app/Atributos_Personajes";
 
     //Metodos
     public boolean existeUsuario(Usuario user){
-        String nombre = user.getNombre();
+        String nombre = user.getNum_Registro();
         File file_ToRead = new File(this.locationUsuario);
         Set<String> fileNames = this.searchFiles(file_ToRead);
-        return fileNames.contains(nombre);
+        return fileNames.contains(nombre +".txt");
     }
 
+    //Usurio
     private Set<String> searchFiles(File file) {
         Set<String> mySearch = new HashSet<String>();
         File[] list_Files = file.listFiles();
@@ -27,6 +30,50 @@ public class FileController {
             }
         }
         return mySearch;
+    }
+
+    public void deleteUsuario(Usuario user){
+        String zone = this.locationUsuario + "/" + user.getNum_Registro() + ".txt";
+        if(existeUsuario(user)){
+            File file_ToDestroy = new File(zone);
+            if(file_ToDestroy.delete()){
+                System.out.println("El archivo "+user.getNombre() +" fue borrado correctamente");
+            }else {
+                System.out.println("error");
+            }
+        }else {
+            System.out.println("No existe este usuario");
+        }
+    }
+
+    private void addAllInfoUser(Usuario user,File ubic) {
+        try{
+            FileWriter userWriter = new FileWriter(ubic);
+            userWriter.write("Nombre : " +user.getNombre());
+            userWriter.write("NickName : "+user.getNick());
+            userWriter.write("Password : "+user.getPassword());
+            userWriter.write("Numero de reesgitro : "+user.getNum_Registro());
+            userWriter.write("Rol : "+ user.getRol());
+            userWriter.write("Personaje : "+user.getPersonajeActivo().getNombre());
+
+        } catch (IOException e) {
+            System.out.println("Exception :: "+e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addUsuario(Usuario usuario){
+        String zonaUser = this.locationUsuario + "/" + usuario.getNum_Registro() + ".txt";
+        try {
+            File newUser = new File(zonaUser);
+            if (newUser.createNewFile()){
+                System.out.println("El usuario "+ usuario.getNombre() +" ha sido creado");
+                addAllInfoUser(usuario,newUser);
+            }
+        } catch (IOException ioe) {
+            System.out.println("Error : " + ioe);
+            throw new RuntimeException(ioe);
+        }
     }
 
 
