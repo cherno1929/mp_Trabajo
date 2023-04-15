e5bb8a7018d72ad834c297cd84913d41114c2831
Cherno Valdor <y.labchiri.2021@alumnos.urjc.es>
2023-04-15 16:16:25 +0200

    Algunos cambios pequeños

--- src/FileController.java
+++ src/FileController.java
@@ -1,13 +1,34 @@
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class FileController {
 
     //Atributes
-    private String locationUsuario = "Ficheros_app/Atributos_Personajes";
+    private String locationUsuario = "Ficheros_app/Usuarios";
     private String locationDesafios = "Ficheros_app/Desafios";
     private String localPersoanjes = "Ficheros_app/Personaje_Usuario";
     private String LocalAtributos = "Ficheros_app/Atributos_Personajes";
 
+    //Metodos
+    public boolean existeUsuario(Usuario user){
+        String nombre = user.getNombre();
+        File file_ToRead = new File(this.locationUsuario);
+        Set<String> fileNames = this.searchFiles(file_ToRead);
+        return fileNames.contains(nombre);
+    }
 
-    
+    private Set<String> searchFiles(File file) {
+        Set<String> mySearch = new HashSet<String>();
+        File[] list_Files = file.listFiles();
+        if(list_Files != null){
+            for(File fil : list_Files){
+                mySearch.add(fil.getName());
+            }
+        }
+        return mySearch;
+    }
 
 
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -9,5 +9,82 @@ public class Usuario {
     public Personaje personajeActivo;
     public AppController app;
 
+    //Metodods
 
+
+    //Cosntructor
+
+    public Usuario(String nombre, String nick, String password, String num_Registro, Rol rol, Personaje personajeActivo, AppController app) {
+        this.nombre = nombre;
+        this.nick = nick;
+        this.password = password;
+        this.num_Registro = num_Registro;
+        this.rol = rol;
+        this.personajeActivo = personajeActivo;
+        this.app = app;
+    }
+
+    public Usuario() {
+
+    }
+
+
+    //Get-Set
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNum_Registro() {
+        return num_Registro;
+    }
+
+    public void setNum_Registro(String num_Registro) {
+        this.num_Registro = num_Registro;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Personaje getPersonajeActivo() {
+        return personajeActivo;
+    }
+
+    public void setPersonajeActivo(Personaje personajeActivo) {
+        this.personajeActivo = personajeActivo;
+    }
+
+    public AppController getApp() {
+        return app;
+    }
+
+    public void setApp(AppController app) {
+        this.app = app;
+    }
 }
