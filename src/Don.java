e657b59e9072a76a595778f9a820823503abafae
Ángel Murciano Baena <a.murciano.2021@alumnos.urjc.es>
2023-04-15 19:39:57 +0200

    Fix: Los dones ahora pueden otorgar rabia (Requisito)

--- src/Don.java
+++ src/Don.java
@@ -3,11 +3,13 @@ public class Don extends Habilidad_Especial{
     //Atributos
     private int umbral;
 
+    private int rabia_Otorgada;
     //Constructor
 
-    public Don(String nombre, int atk, int def, int umbral) {
+    public Don(String nombre, int atk, int def, int umbral, int rabia_Otorgada) {
         super(nombre, atk, def);
         this.umbral = umbral;
+        this.rabia_Otorgada = rabia_Otorgada;
     }
 
     //Get-Set
@@ -18,5 +20,13 @@ public class Don extends Habilidad_Especial{
 
     public void setUmbral(int umbral) {
         this.umbral = umbral;
+    }
+
+    public int getRabia_Otorgada() {
+        return rabia_Otorgada;
+    }
+
+    public void setRabia_Otorgada(int rabia_Otorgada) {
+        this.rabia_Otorgada = rabia_Otorgada;
     }
 }
