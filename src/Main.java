67af3fa6032a965ee2f1c40dd913f768c24d27c1
Ángel Murciano Baena <a.murciano.2021@alumnos.urjc.es>
2023-06-25 17:40:20 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import org.junit.jupiter.api.Test;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    @Test
     public static void main(String[] args) {
         AppController ac = new AppController();
         ac.Menu();
